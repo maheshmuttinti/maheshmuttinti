e58f44a34e3d38de3f6079f75b165954f882ca92
maheshmuttinti <maheshmuttinti@betalectic.com>
2022-11-30 20:21:06 +0530

    sync changes

---
+++ mobile-screens/screens/Development/CAMSVerification/OTPVerification.js
@@ -0,0 +1,218 @@
+/* eslint-disable react-native/no-inline-styles */
+import React, {useState, useEffect, useRef} from 'react';
+import {View} from 'react-native';
+import {GrayBodyText, AuthHeading, CustomOtpInput} from 'uin';
+import AuthWrapper from '../../../hocs/AuthWrapper';
+import useBetaForm from '@reusejs/react-form-hook';
+import {
+  loginWithOTP,
+  getUser,
+  requestForLoginOTP,
+  updateUserProfile,
+} from 'services';
+import {setTokens, setUser} from 'store';
+import {useDispatch} from 'react-redux';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import BackgroundTimer from '../../../reusables/BackgroundTimer';
+import {useTheme} from 'theme';
+
+export const OTPVerification = ({route, navigation}) => {
+  const [infoText, setInfoText] = useState('');
+  const [checkLoginStatus, setCheckLoginStatus] = useState('not_logged_in');
+  const value = route?.params?.value;
+  const type = route?.params?.type;
+  const dispatch = useDispatch();
+  const handleLogin = useRef(() => {});
+  const persistLogin = useRef(() => {});
+  const updateUserProfileFnRef = useRef(() => {});
+  const theme = useTheme();
+
+  const form = useBetaForm({
+    type: type,
+    value: value,
+    token: '',
+  });
+
+  useEffect(() => {
+    if (type === 'mobile_number') {
+      setInfoText(`FinEzzy has sent SMS with a verification code to ${value}`);
+    } else {
+      setInfoText(
+        `FinEzzy has sent an email with a verification code to ${value}`,
+      );
+    }
+  }, [type, value]);
+
+  updateUserProfileFnRef.current = async user => {
+    try {
+      console.log('verifySigninUsingOtp - user', JSON.stringify(user, null, 2));
+
+      let usernameType = null;
+      if (type === 'email') {
+        usernameType = 'non_gmail';
+      }
+      if (type === 'mobile_number') {
+        usernameType = 'mobile_number';
+      }
+      const meta = {
+        meta: {...user?.profile?.meta, username_type: usernameType},
+      };
+
+      console.log(
+        'updateProfilePayload in verifySigninUsingOtp screen',
+        updateProfilePayload,
+      );
+
+      const updateProfilePayload = {
+        ...meta,
+      };
+      const updateProfileResponse = await updateUserProfile(
+        updateProfilePayload,
+      );
+      if (updateProfileResponse) {
+        console.log(
+          'updated the profile in verify signin using otp screen adn redirecting to screen determinor screen',
+        );
+        navigation.replace('Auth', {
+          screen: 'ScreenDeterminor',
+        });
+      }
+    } catch (err) {
+      console.log('error', err);
+    }
+  };
+
+  persistLogin.current = async () => {
+    try {
+      const tokenFromStorage = await AsyncStorage.getItem('@access_token');
+      if (checkLoginStatus === 'logged_in') {
+        if (tokenFromStorage !== null) {
+          dispatch(setTokens(JSON.parse(tokenFromStorage)));
+          let user = await getUser();
+          console.log('user', JSON.stringify(user, null, 2));
+          dispatch(setUser(user));
+          updateUserProfileFnRef.current(user);
+        }
+      }
+    } catch (error) {
+      console.log('error', error);
+      navigation.replace('Auth', {screen: 'SigninHome'});
+    }
+  };
+
+  useEffect(() => {
+    persistLogin.current();
+  }, [checkLoginStatus]);
+
+  useEffect(() => {
+    if (form.value.token.length === 6) {
+      handleLogin.current();
+    }
+  }, [form.value.token]);
+
+  handleLogin.current = async () => {
+    try {
+      console.log('form.value', form.value);
+      const payload = {
+        type: form.value.type,
+        value: form.value.value,
+        token: form.value.token,
+      };
+
+      console.log('login with otp payload', payload);
+      const response = await loginWithOTP(payload);
+      console.log(
+        'access_token response in verify signin using otp',
+        JSON.stringify(response, null, 2),
+      );
+
+      if (response?.access_token) {
+        await AsyncStorage.setItem(
+          '@access_token',
+          JSON.stringify({
+            accessToken: response?.access_token,
+          }),
+        );
+        await AsyncStorage.setItem('@loggedin_status', JSON.stringify(true));
+        setCheckLoginStatus('logged_in');
+      }
+    } catch (error) {
+      console.log('error', error);
+      if (error?.errors?.token[0] === 'Please provide token') {
+        form.setErrors({token: 'Please enter verification code'});
+      } else if (error?.message === 'Invalid Token') {
+        console.log('error', error);
+        form.setErrors({token: 'Please enter a valid code.'});
+      }
+    }
+  };
+
+  const handleResendOTP = async () => {
+    try {
+      console.log('form.value', form.value);
+      const payload = {
+        type: form.value.type,
+        value:
+          form.value.type === 'mobile_number'
+            ? form.value.value.slice(3)
+            : form.value.value,
+        token: form.value.token,
+      };
+      console.log('payload for resend otp -login', payload);
+      const resendOTPResponse = await requestForLoginOTP(payload);
+      console.log('response', JSON.stringify(resendOTPResponse, null, 2));
+    } catch (error) {
+      console.log('error', error);
+      if (error?.message === 'Invalid Token') {
+        console.log('error', error);
+        form.setErrors({token: 'Please enter a valid code.'});
+      } else {
+        form.setErrors(error);
+      }
+    }
+  };
+
+  return (
+    <AuthWrapper>
+      <AuthHeading>Verification Code</AuthHeading>
+
+      <View style={{paddingTop: 16}}>
+        <GrayBodyText>{infoText}</GrayBodyText>
+      </View>
+
+      <View style={{paddingTop: 24}}>
+        <CustomOtpInput
+          defaultValue={''}
+          onChangeText={text => form.setField('token', text)}
+          value={form.getField('token')}
+          error={form.errors.get('token')}
+          textInputStyles={
+            form.errors.get('token')
+              ? {
+                  borderColor: theme.colors.error,
+                  backgroundColor: theme.colors.greyscale50,
+                }
+              : {
+                  backgroundColor: theme.colors.greyscale50,
+                  borderColor: 'transparent',
+                }
+          }
+          tintColor={
+            form.errors.get('token')
+              ? theme.colors.error
+              : theme.colors.primaryBlue
+          }
+          // left={-21}
+          // scale={0.9}
+          // inputCount={1}
+          secureTextEntry={false}
+        />
+      </View>
+
+      <BackgroundTimer
+        wrapperStyles={{paddingTop: 32}}
+        callBack={() => handleResendOTP()}
+      />
+    </AuthWrapper>
+  );
+};
--- mobile-screens/screens/Development/index.js
+++ mobile-screens/screens/Development/index.js
@@ -1,15 +1,16 @@
 /* eslint-disable react-native/no-inline-styles */
 import {View} from 'react-native';
 import React from 'react';
-import {BaseButton, Stepper, useStepper} from 'uin';
+import {Stepper, useStepper} from 'uin';
 import SplashScreen from 'react-native-splash-screen';
-import {RadioCircleFill, RadioCircleOutline, TickCircle} from 'assets';
-import Test from './Test';
+import {BlueDotCircle, GreyCircle, GreenTickCircleSmall} from 'assets';
 import {useTheme} from 'theme';
+import {CollectMobileAndEmail} from './CAMSVerification/CollectMobileAndEmail';
+import ScreenWrapper from '../../hocs/screen_wrapper';
 
 const SCREEN_BACKGROUND_COLOR = 'white';
-const ICON_HEIGHT = 30;
-const ICON_WIDTH = 30;
+const ICON_HEIGHT = 16;
+const ICON_WIDTH = 16;
 
 export default function () {
   const theme = useTheme();
@@ -21,48 +22,33 @@ export default function () {
     useStepper();
 
   return (
-    <View style={{backgroundColor: SCREEN_BACKGROUND_COLOR, flex: 1}}>
-      <Test />
-      <View style={{marginTop: 40}}>
-        <Stepper
-          iconHeight={ICON_HEIGHT}
-          iconWidth={ICON_WIDTH}
-          iconBackgroundColor={SCREEN_BACKGROUND_COLOR}
-          notSelectedComponent={() => <RadioCircleOutline />}
-          checkedComponent={() => <TickCircle />}
-          selectedComponent={() => <RadioCircleFill />}
-          activeLineColor="blue"
-          checkedLabelColor={theme.colors.primaryBlue}
-          selectedLabelColor="black"
-          notSelectedLabelColor="grey"
-          hideDefaultHeader={false}
-          wrapperPadding={24}
-          activeStep={2}
-          capitalizeLabel={true}>
-          <Stepper.Steps>
-            <Stepper.Step id="first" name="Holdings- Cams" />
-            <Stepper.Step id="second" name="Holdings- Karvy" />
-            <Stepper.Step id="third" name="LIEN MARK- CAMS" />
-            <Stepper.Step id="four" name="LIEN MARK- Karvy" />
-          </Stepper.Steps>
-        </Stepper>
-        {/* <View style={{flexDirection: 'row', marginTop: 64}}>
-          <BaseButton
-            onPress={() => {
-              decrementCurrentStep(currentStep - 1);
-            }}
-            style={{flex: 1 / 2, marginRight: 12}}>
-            Previous Step
-          </BaseButton>
-          <BaseButton
-            onPress={() => {
-              incrementCurrentStep(currentStep + 1);
-            }}
-            style={{flex: 1 / 2}}>
-            Next Step
-          </BaseButton>
-        </View> */}
+    <ScreenWrapper style={{backgroundColor: SCREEN_BACKGROUND_COLOR}}>
+      <View style={{paddingHorizontal: 24, flex: 1}}>
+        <View style={{marginTop: 40}}>
+          <Stepper
+            iconHeight={ICON_HEIGHT}
+            iconWidth={ICON_WIDTH}
+            iconBackgroundColor={SCREEN_BACKGROUND_COLOR}
+            notSelectedComponent={() => <GreyCircle />}
+            checkedComponent={() => <GreenTickCircleSmall />}
+            selectedComponent={() => <BlueDotCircle />}
+            activeLineColor="blue"
+            checkedLabelColor={theme.colors.primaryBlue}
+            selectedLabelColor="black"
+            notSelectedLabelColor="grey"
+            hideDefaultHeader={false}
+            activeStep={0}
+            capitalizeLabel={true}>
+            <Stepper.Steps>
+              <Stepper.Step id="first" name="CAMS" />
+              <Stepper.Step id="second" name="KARVY" />
+            </Stepper.Steps>
+          </Stepper>
+        </View>
+        <View style={{marginTop: 64, flex: 1}}>
+          <CollectMobileAndEmail onSubmit={() => incrementCurrentStep()} />
+        </View>
       </View>
-    </View>
+    </ScreenWrapper>
   );
 }
