3bb7dd0b4cecb67e20da97a8fd804f5976e8abc0
maheshmuttinti <maheshmuttinti@betalectic.com>
2022-12-01 13:27:24 +0530

    sync changes

--- mobile-screens/screens/Development/index.js
+++ mobile-screens/screens/Development/index.js
@@ -1,117 +1,31 @@
 /* eslint-disable react-native/no-inline-styles */
 import {Pressable, View, Text} from 'react-native';
-import React, {useState, useMemo} from 'react';
-import {Stepper, useStepper} from 'uin';
-import SplashScreen from 'react-native-splash-screen';
-import {
-  BlueDotCircle,
-  GreyCircle,
-  GreenTickCircleSmall,
-  BackArrow,
-} from 'assets';
+import React from 'react';
 import {useTheme} from 'theme';
-import ScreenWrapper from '../../hocs/screen_wrapper';
-import {CollectMobileAndEmail} from './FetchCASFromRTAs/CAMS/CollectMobileAndEmail';
-import {OTPVerification as CAMSOTPVerification} from './FetchCASFromRTAs/CAMS/OTPVerification';
-import {OTPVerification as KarvyOTPVerification} from './FetchCASFromRTAs/Karvy/OTPVerification';
+import CombinedUpdateCASAndLienMarking from './CombinedUpdateCASAndLienMarking';
+import FetchCASFromRTAsForm from './FetchCASFromRTAs/StepperForm';
 
-const SCREEN_BACKGROUND_COLOR = 'white';
-const ICON_HEIGHT = 16;
-const ICON_WIDTH = 16;
+// const screens = ['FetchCASFromRTAsForm'];
 
 export default function ({navigation}) {
-  const [redirectToCAMSOTPVerification, setRedirectToCAMSOTPVerification] =
-    useState(false);
   const theme = useTheme();
-  React.useEffect(() => {
-    SplashScreen.hide();
-  }, []);
-
-  const {incrementCurrentStep, decrementCurrentStep, currentStep, steps} =
-    useStepper();
-  const backArrowIconWrapperStyle = {
-    paddingTop: 48,
-  };
-
-  const handleGoBack = () => {
-    if (redirectToCAMSOTPVerification === true) {
-      setRedirectToCAMSOTPVerification(false);
-    } else if (!redirectToCAMSOTPVerification && currentStepToShow === 2) {
-      setRedirectToCAMSOTPVerification(true);
-      decrementCurrentStep();
-    }
-  };
-
-  const currentStepToShow = useMemo(
-    () => (currentStep + 1 > steps?.length ? steps.length : currentStep + 1),
-    [currentStep, steps],
-  );
-
   return (
-    <ScreenWrapper style={{backgroundColor: SCREEN_BACKGROUND_COLOR}}>
-      <View style={{paddingHorizontal: 24, flex: 1}}>
-        <View style={{...backArrowIconWrapperStyle}}>
-          <Pressable
-            hitSlop={{top: 30, left: 30, right: 30, bottom: 30}}
-            onPress={() => {
-              handleGoBack();
-            }}
-            style={{width: 50}}>
-            <BackArrow />
-          </Pressable>
-        </View>
-        <View style={{marginTop: 16}}>
-          <Stepper
-            iconHeight={ICON_HEIGHT}
-            iconWidth={ICON_WIDTH}
-            iconBackgroundColor={SCREEN_BACKGROUND_COLOR}
-            notSelectedComponent={() => <GreyCircle />}
-            checkedComponent={() => <GreenTickCircleSmall />}
-            selectedComponent={() => <BlueDotCircle />}
-            activeLineColor="blue"
-            checkedLabelColor={theme.colors.primaryBlue}
-            selectedLabelColor="black"
-            notSelectedLabelColor="grey"
-            hideDefaultHeader={false}
-            activeStep={0}
-            capitalizeLabel={true}>
-            <Stepper.Steps>
-              <Stepper.Step id="first" name="CAMS" />
-              <Stepper.Step id="second" name="KARVY" />
-            </Stepper.Steps>
-          </Stepper>
-        </View>
-        <View style={{marginTop: 64, flex: 1}}>
-          <Text
-            style={{
-              ...theme.fontSizes.small,
-              fontWeight: theme.fontWeights.moreBold,
-              color: theme.colors.primaryOrange,
-              fontFamily: theme.fonts.regular,
-              paddingBottom: 8,
-            }}>
-            {`VERIFICATION ${currentStepToShow} of ${steps?.length}`}
-          </Text>
-          {currentStepToShow === 1 && !redirectToCAMSOTPVerification && (
-            <CollectMobileAndEmail
-              onSubmit={() => {
-                setRedirectToCAMSOTPVerification(true);
-              }}
-            />
-          )}
-          {redirectToCAMSOTPVerification === true && (
-            <CAMSOTPVerification
-              onSubmit={() => {
-                setRedirectToCAMSOTPVerification(false);
-                incrementCurrentStep();
-              }}
-            />
-          )}
-          {!redirectToCAMSOTPVerification && currentStepToShow === 2 && (
-            <KarvyOTPVerification onSubmit={() => incrementCurrentStep()} />
-          )}
-        </View>
-      </View>
-    </ScreenWrapper>
+    <>
+      {/* {screens.map((screen, index) => (
+        <Pressable
+          onPress={() => {
+            console.log('pressed');
+            navigation.replace('Development', {screen: screen});
+          }}
+          key={`screen-${index}`}
+          style={{backgroundColor: 'violet', padding: 16}}>
+          <Text style={{...theme.fontSizes.heading5}}>{screen}</Text>
+        </Pressable>
+      ))} */}
+
+      {/* <FetchCASFromRTAsForm /> */}
+
+      <CombinedUpdateCASAndLienMarking />
+    </>
   );
 }
--- mobile-screens/stacks/Development.js
+++ mobile-screens/stacks/Development.js
@@ -1,16 +1,29 @@
 import * as React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
-import RTAsPortfoliosFetchScreen from '../screens/Development';
+import DevelopmentList from '../screens/Development';
+import FetchCASFromRTAsForm from '../screens/Development/FetchCASFromRTAs/StepperForm';
+import CombinedUpdateCASAndLienMarking from '../screens/Development/CombinedUpdateCASAndLienMarking';
 
 const Development = () => {
   const Stack = createStackNavigator();
 
   return (
-    <Stack.Navigator initialRouteName={RTAsPortfoliosFetchScreen}>
+    <Stack.Navigator initialRouteName={DevelopmentList}>
       <Stack.Screen
-        name="RTAsPortfoliosFetchScreen"
+        name="DevelopmentList"
         options={{headerShown: false}}
-        component={RTAsPortfoliosFetchScreen}
+        component={DevelopmentList}
+      />
+      <Stack.Screen
+        name="FetchCASFromRTAsForm"
+        options={{headerShown: false}}
+        component={FetchCASFromRTAsForm}
+      />
+
+      <Stack.Screen
+        name="CombinedUpdateCASAndLienMarking"
+        options={{headerShown: false}}
+        component={CombinedUpdateCASAndLienMarking}
       />
     </Stack.Navigator>
   );
